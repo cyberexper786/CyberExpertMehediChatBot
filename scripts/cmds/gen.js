39020e3586fb87c3d79fb772691f81b0504fc6e7
ᴍᴇʜᴇᴅɪ ᴋʜᴀɴ <maishaprojectofficial6@gmail.com>
2026-09-10 01:01:13 +0600

    Update gen.js

--- scripts/cmds/gen.js
+++ scripts/cmds/gen.js
@@ -12,7 +12,7 @@ const config = {
   name: "gen",
   aliases: ["xdis"],
   version: "2.2",
-  author: "ai",
+  author: "rX",
   role: 0,
   description: "Generate AI video using your own prompt",
   category: "fun",
