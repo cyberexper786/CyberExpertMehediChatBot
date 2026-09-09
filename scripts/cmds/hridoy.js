256b2d5f7dfa26f39a09a3af5582fb6d358f5918
ᴍᴇʜᴇᴅɪ ᴋʜᴀɴ <maishaprojectofficial6@gmail.com>
2026-09-09 18:00:01 +0600

    Update adminUID to remove part of a UID

--- scripts/cmds/hridoy.js
+++ scripts/cmds/hridoy.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 
 const cooldowns = new Map();
 
-const adminUID = ["61594324973630", "61583147223219"];
+const adminUID = ["615943", "61583147223219"];
 
 module.exports = {
   config: {
